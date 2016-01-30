7ef60ef6b18af40a290f402a3821a64e84ed050b
giangm9 <giangm9@gmail.com>
2016-01-30 11:28:39 +0700

    more easy to crawl

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import requests
 import json
 
-
-def getPage(offset):    
-    print offset
-    url = 'https://cymon.io/api/nexus/v1/blacklist/ip/dnsbl/?days=3&limit=100000'
+def getCymonPage(tag, day, limit, offset, filename):
+    print 'day   : ' + day + '   offset    : ' + offset
+    print 'limit : ' + limit + ' file name : ' + filename
+    url = 'https://cymon.io/api/nexus/v1/blacklist/ip/'+ tag + '/?days=3&limit=100000'
+    url += '/?days=' + day
+    url += '&limit=' + limit
+    url += '&offset=' + offset
+    
+    
     r = requests.get(url)
     raw = json.loads(r.text)
     result = raw['results']
     
-    fo = open('cymon/dnsbl.txt', 'w')
+    fo = open('cymon/' + filename + '.txt', 'w')
+        
     
-    for item in result:
-        print item
+    for item in result: 
+        print '.',
         fo.write(item['url'] + '\n' + item['addr'] + '\n\n')
     
-    fo.close()
-    print 'finish' + offset
-
-
-getPage('')
-        
-
-
+    fo.close()    
+    print '\ndone, total : ' + str(len(result))
+    
+getCymonPage('malware', '3', '10000', '0', 'malware')
+getCymonPage('botnet', '3', '10000', '0', 'botnet')    
