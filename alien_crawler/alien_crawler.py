3a4a174049e625ed1655a4b78a6d2ab30530631a
Minh Giang <tess200295@gmail.com>
2016-02-16 22:53:15 +0700

    debug

--- alien_crawler/alien_crawler.py
+++ alien_crawler/alien_crawler.py
@@ -12,14 +12,15 @@ for i in range (1,5):
     url = "https://otx.alienvault.com/otxapi/search/?q=&sort=null&limit=20&page=" + str(i)
     r = requests.get(url)
     raw = json.loads(r.text)
-    id = raw['results'][0]['id']
 
     #crawl all threads in day
     for i in range(0,19):
         name = u'\u2019'
         name = raw['results'][i]['name']
+        id = raw['results'][i]['id']
+        
         if raw['results'][i]['modified'][0:10] == str(today)[0:10]:
-            f.write("name: " + smart_str(name) + "\n")
+            f.write("name: " + smart_str(name) + "(id: " + str(id) + ")" + "\n")
             f.write("created time: " + raw['results'][i]['created'] + "\n")
             f.write("modified time: " + raw['results'][i]['modified'] + "\n")
             f.write("Detail:" + "\n")
@@ -30,7 +31,7 @@ for i in range (1,5):
             inside_raw = json.loads(inside_r.text)
 
             #crawk info from each thread
-            for i in range(0,inside_raw['count']):
+            for i in range(0, inside_raw['count']):
                 f.write("     type: " + inside_raw['results'][i]['type'] + "\n")
                 f.write("     indicator: " + inside_raw['results'][i]['indicator'] + "\n")
                 f.write("     time: " + inside_raw['results'][i]['created'] + "\n")
