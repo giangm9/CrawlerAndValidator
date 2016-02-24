7932576c772188db823f96d71f2574ca90d69bf2
anhnt_58 <anhnt_58@vnu.edu.vn>
2016-02-24 23:34:56 +0700

    re-construct

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Crawler and Validator',
+      version='1.0',
+      description='Crawler and Validator to work with informations from AllienVault and Cymon',
+      author='BigForce',
+      url='https://github.com/giangm9/CymonCrawler',
+      packages=['CrawlerAndValidator'],
+     )
