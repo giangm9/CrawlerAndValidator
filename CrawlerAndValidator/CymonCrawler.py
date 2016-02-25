14b3a7b67eb0b5356cfad2209d1db75efb701c89
giangm9 <giangm9@gmail.com>
2016-02-25 11:06:08 +0700

    update CymonCrawler

--- CrawlerAndValidator/CymonCrawler.py
+++ CrawlerAndValidator/CymonCrawler.py
@@ -6,12 +6,15 @@ import DBHelper
 
 
 def crawl(days, tags, adrtypes):
-    print 'Crawling from cymon.io...'
-    print 'j'
+    print 'Crawling from cymon.io...'    
     for day in days:
         for tag in tags:
             for adrtype in adrtypes:
-                getCymonPage(str(day), tag, adrtype, '100000', '0')
+                for i in range(10):                    
+                    limit = 10000
+                    count = getCymonPage(str(day), tag, adrtype, str(limit), str(i*1000))
+                    if (limit * i) > count :
+                        break;
 
         
 def getCymonPage( day, tag, adrtype, limit, offset):                
@@ -21,16 +24,24 @@ def getCymonPage( day, tag, adrtype, limit, offset):
         url += '&limit=' + limit
         url += '&offset=' + offset
         print 'URL: ', url
-        r = requests.get(url)
+        
+        headers = {'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+           'accept-encoding':'gzip, deflate, sdch',
+           'accept-language':'vi,en;q=0.8',
+           'cache-control':'max-age=0',
+           'cookie':'__utmt=1; VT_PREFERRED_LANGUAGE=en; __utma=194538546.508165789.1453801379.1454133997.1454134465.7; __utmb=194538546.18.10.1454134465; __utmc=194538546; __utmz=194538546.1454134465.7.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)',
+           'upgrade-insecure-requests':'1',
+           'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36}'}
+        r = requests.get(url, headers)
         raw = json.loads(r.text)
-        result = raw['results']                
-        for item in result:
-            print '.'
-            
+        result = raw['results']
+        if len(result) == 0:
+            print '\t\b'
+        for item in result:                        
             ShowProgress.show(result.index(item) + 1, len(result))
             if not ('addr' in item.keys()):
                 item['addr'] = item['name']
             DBHelper.updateAddress({'address': item['addr'], 
                                     'address_type': adrtype})
-            print '\b'
         
+        return raw['count']    
